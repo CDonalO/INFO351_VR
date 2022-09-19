f4d2bc7441cf1ee25bbde3a81cf582f36f9af549
Cordell O'Leary <oleco300@student.otago.ac.nz>
2022-09-19 12:51:26 +1200

    added more :)

--- Assets/Scripts/Input/BoardInputHandler.cs
+++ Assets/Scripts/Input/BoardInputHandler.cs
@@ -1,20 +1,20 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// [RequireComponent(typeof(Board))]
-// public class BoardInputHandler : MonoBehaviour, IInputHandler
-// {
-//     private Board board;
+[RequireComponent(typeof(Board))]
+public class BoardInputHandler : MonoBehaviour, IInputHandler
+{
+    private Board board;
 
-//     private void Awake()
-//     {
-//         board = GetComponent<Board>();
-//     }
+    private void Awake()
+    {
+        board = GetComponent<Board>();
+    }
 
-//     public void ProcessInput(Vector3 inputPosition, GameObject selectedObject, Action onClick)
-//     {
-//         board.OnSquareSelected(inputPosition);
-//     }
-// }
+    public void ProcessInput(Vector3 inputPosition, GameObject selectedObject, Action onClick)
+    {
+        board.OnSquareSelected(inputPosition);
+    }
+}
